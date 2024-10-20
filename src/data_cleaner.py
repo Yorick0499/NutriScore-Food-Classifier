4947f3d40b197195003b2d2742a0af66cd613d58
Yorick0499 <kacper11812@gmail.com>
2024-10-20 15:49:22 +0200

    Fixed bugs

--- src/data_cleaner.py
+++ src/data_cleaner.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import nutriscore_creator
 
-path = '../data/food_table.csv'
+path = 'data/food_data.csv'
 data = pd.read_csv(path)
 
 data = data.rename(columns={'Calories (kcal)':'Calories',
@@ -31,4 +31,4 @@ data = data.drop(columns=['Sugar_points','Fat_points','Protein_points',
 # print(data[['Producer','Name','Sugar_points','Fat_points','Protein_points','Salt_points','Carbo_points','Nutriscore digit']].loc[data['Nutriscore']=='B'])
 # print(data['Carbo_points'].unique())
 # print(data.groupby('Nutriscore')['Price'].mean().round(2))
-# print(data[['Name','Nutriscore digit','Nutriscore']])
+print(data[['Name','NutriScore']])
